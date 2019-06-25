e5f4edef4af55302b37c92dfea6548818d38a763
vagrant <vagrant@homestead>
2019-06-25 09:14:53 +0000

    Add user following

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,8 +18,8 @@ class SessionsController extends Controller
     
     public function create() 
     {
-
-    	return view('sessions.create');
+    
+      return view('sessions.create');
     }
 
     public function store(Request $request) 
